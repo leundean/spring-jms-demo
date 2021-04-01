e299a329fb4db60efcbd98efc39be6e361b37b38
Mikael_Lundin <mikael@leundean.com>
2021-04-01 09:19:04 +0200

    package

--- storage/src/main/java/storage/Application.java
+++ storage/src/main/java/storage/Application.java
@@ -1,4 +1,4 @@
-package springjmsdemo;
+package storage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- storage/src/main/java/storage/controller/ManualController.java
+++ storage/src/main/java/storage/controller/ManualController.java
@@ -5,12 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import storage.model.Product;
 import storage.service.DistributionService;
 import storage.service.StorageService;
-
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 
